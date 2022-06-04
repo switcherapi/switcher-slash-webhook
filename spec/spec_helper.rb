8e27c7a8fd3afef575a280bde73c1df6e573d233
petruki <31597636+petruki@users.noreply.github.com>
2022-06-03 20:36:53 -0700

    Fixed spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,7 @@
 ENV['RACK_ENV'] = 'test'
 
 require 'simplecov'
-SimpleCov.start 'rails' do
-  if ENV['CI']
-    require 'simplecov-lcov'
-
-    SimpleCov::Formatter::LcovFormatter.config do |c|
-      c.report_with_single_file = true
-      c.single_report_path = 'coverage/lcov.info'
-    end
-
-    formatter SimpleCov::Formatter::LcovFormatter
-  end
-
-  add_filter %w[version.rb initializer.rb]
-end
+SimpleCov.start
 
 require 'webmock/rspec'
 require_relative File.join('..', 'app')
